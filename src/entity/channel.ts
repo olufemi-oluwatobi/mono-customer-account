1086075bd16bda5258207f4d54f63e53fe8085aa
tobi <oluwatobiolufemi1996@gmail.com>
2021-02-23 02:14:32 +0100

    implement organisation creation

--- src/entity/channel.ts
+++ src/entity/channel.ts
@@ -17,6 +17,7 @@ import { User } from "./user"
 
 
 @Entity()
+
 export class Channel {
     @PrimaryGeneratedColumn()
     id: number;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router, Request, Response } from "express";
 import auth from "./auth";
 import user from "./user";
+import organisation from "./organisation";
+
 
 const routes = Router();
 
 routes.use("/auth", auth);
 routes.use("/user", user);
+routes.use("/organisation", organisation)
 
 export default routes;
